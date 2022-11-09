7bbcd46d0db70c180276c3ca9fe7a74468305c18
Freshley Grisyuan <grisyuansurya@gmail.com>
2022-11-09 13:56:06 +0700

    angular day 4

---
+++ angular_learning/src/app/kasir/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit,EventEmitter,Output, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { item } from '../kasir/kasir.component';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  styles:['[bgaq]{  background-color: aquamarine;}',
+  '[coba]{background: red; color: white}'
+]
+})
+export class MenuComponent implements OnInit, AfterViewInit {
+  @Input('items') items: item[]=[]
+  @Output ('itemAdded') onAddItem : EventEmitter<item> = new EventEmitter<item>()
+  @ViewChild('bgaq') bgaq?:ElementRef;
+  @ViewChild('al') bgRed?:ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addItem(item :item) : void{
+    this.onAddItem.emit(item)
+  }
+
+  ngAfterViewInit(){
+
+    this.bgaq?.nativeElement.setAttribute('bgaq', '');
+    this.bgRed?.nativeElement.setAttribute('coba', '');
+   
+  }
+}
